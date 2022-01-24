2305fa25274baad9de7725b958b6867bb2eac4f8
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-01-24 17:03:37 +0300

    edit asbsad

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Dialogs/Dialogs.jsx
@@ -4,9 +4,33 @@ import s from './Dialogs.module.css'
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
-        </div>
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog+' '+s.active}>
+                    Petia
+                    </div>
+                    <div className={s.dialog}>
+                    Valera
+                    </div>
+                    <div className={s.dialog}>
+                    Lyda
+                    </div>
+                    <div className={s.dialog}>
+                    Vasia
+                    </div>
+                    <div className={s.dialog}>
+                    Gavrila
+                    </div>
+                    <div className={s.dialog}>
+                    Oksana
+                    </div>
+            </div>
+        <div className={s.messages}>
+        <div className={s.dialog}>Hi</div>
+        <div className={s.dialog}>Hello, how are you?</div>
+        <div className={s.dialog}>Vasappi</div>
+            </div>
+            </div>
     )
     }
     export default Dialogs;
--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Navbar/Navbar.jsx
@@ -7,17 +7,17 @@ const Navbar = () => {
          <div className={`${s.item} ${s.activeNavLink}`} >   
             <NavLink to="/profile"activeClasseName={s.active}>Profile</NavLink>
         </div>
-        <div className={`${s.item} ${s.active}`}>
+        <div className={`${s.item} ${s.activeNavLink}`}>
             <NavLink to="/dialogs"activeClasseName={s.active}>Messages</NavLink>
         </div>
-        <div className={s.item}>
-            <NavLink to="/news">News</NavLink>
+        <div className={`${s.item} ${s.activeNavLink}`}>
+            <NavLink to="/news"activeClasseName={s.active}>News</NavLink>
         </div>
-        <div className={s.item}>
-            <NavLink to="/music">Music</NavLink>
+        <div className={`${s.item} ${s.activeNavLink}`}>
+            <NavLink to="/music"activeClasseName={s.active}>Music</NavLink>
         </div>
-        <div className={s.item}>
-            <NavLink to="/setting">Settings</NavLink>
+        <div className={`${s.item} ${s.activeNavLink}`}>
+            <NavLink to="/setting"activeClasseName={s.active}>Settings</NavLink>
         </div>
     </nav>
 }
