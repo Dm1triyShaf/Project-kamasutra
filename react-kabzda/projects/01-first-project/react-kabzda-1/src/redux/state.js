4a25ce2e76072f3b629b51185fc0774468a6a55d
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-02-05 02:22:00 +0300

    edit asdsad

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/redux/state.js
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/redux/state.js
@@ -1,4 +1,7 @@
-import { rerenderEntireTree } from "../render";
+
+let rerenderEntireTree = () => {
+console.log('State changet');
+}
 
 let state = {
     profilePage: {
@@ -30,7 +33,7 @@ let state = {
 
     window.state=state;
 
-    export let addPost = ()=>{
+    export const addPost = ()=>{
         let newPost = {
             id: 5,
             message: state.profilePage.newPostText,
@@ -42,9 +45,14 @@ let state = {
     rerenderEntireTree (state);
     }
 
-    export let updateNewPostText = (newText)=>{  
+    export const updateNewPostText = (newText)=>{  
     state.profilePage.newPostText = newText;
     rerenderEntireTree (state);
+    }
+
+    export const subscribe = (observer) => {
+        rerenderEntireTree = observer;
+
     }
 
     export default state;
