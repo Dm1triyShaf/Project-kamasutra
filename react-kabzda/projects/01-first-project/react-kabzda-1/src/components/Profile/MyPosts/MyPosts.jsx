61382f55966b5929f720c724ea9a41f7008f0d2f
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-02-10 18:58:52 +0300

    edit asdsad

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { addPostActionCreator, updateNewPostTextActionCreator } from '../../../redux/state';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
+
+
+
 
 const MyPosts = (props) => {
   let postsElements =
@@ -9,13 +13,14 @@ const MyPosts = (props) => {
   let nevPostElement = React.createRef();
 
   let addPost = () => {
-    props.dispatch({ type: 'ADD-POST' });
+    props.dispatch(addPostActionCreator());
 
   }
 
   let onPostChange = () => {
     let text = nevPostElement.current.value;
-    let action = ({ type: 'UPDATE-NEW-POST-TEXT', newText: (text) });
+    // let action = ({ type: 'UPDATE-NEW-POST-TEXT', newText: (text) });
+    let action = updateNewPostTextActionCreator(text)
     props.dispatch(action);
   }
 
