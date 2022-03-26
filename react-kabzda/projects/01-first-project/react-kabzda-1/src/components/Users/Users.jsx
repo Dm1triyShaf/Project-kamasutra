0d685da366d0fb00152b2df07f1cad32b5a445c9
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-03-26 16:04:01 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
@@ -5,13 +5,16 @@ import userPhoto from '../../../src/assets/images/images.jfif';
 
 let Users = (props) => {
     let getUsers = () =>
-    if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items);
-        });
+    {
+        if (props.users.length === 0) {
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items);
+            });
+        }
     }
 
     return <div>
+        <button onClick = {getUsers}>Get Users</button>
         {props.users.map(u => <div key={u.id}>
 <span>
     <div>
