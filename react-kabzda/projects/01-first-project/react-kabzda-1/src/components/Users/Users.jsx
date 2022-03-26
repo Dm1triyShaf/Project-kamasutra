b8e3acb903e3f915b9985b6c049d18d5ddb693e7
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-03-26 17:03:12 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
@@ -3,32 +3,33 @@ import styles from './users.module.css';
 import * as axios from 'axios';
 import userPhoto from '../../../src/assets/images/images.jfif';
 
-let Users = (props) => {
-    let getUsers = () =>
+class Users extends  React.Component {
+    getUsers = () =>
     {
-        if (props.users.length === 0) {
+        if (this.props.users.length === 0) {
             axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                props.setUsers(response.data.items);
+                this.props.setUsers(response.data.items);
             });
         }
     }
 
-    return <div>
-        <button onClick = {getUsers}>Get Users</button>
-        {props.users.map(u => <div key={u.id}>
+    render() {
+       return <div>
+            <button onClick = {this.getUsers}>Get Users</button>
+            {this.props.users.map(u => <div key={u.id}>
 <span>
     <div>
         <img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
     </div>
     <div>
         {u.followed ? <button onClick={() => {
-            props.unfollow(u.id)
+            this.props.unfollow(u.id)
         }}>Unfollow</button> : <button onClick={() => {
-            props.follow(u.id)
+            this.props.follow(u.id)
         }}>Follow</button>}
     </div>
 </span>
-            <span>
+                <span>
                 <span>
                     <div>{u.name}</div>
                     <div>{u.status}</div>
@@ -38,8 +39,10 @@ let Users = (props) => {
                     <div>{"u.location.city"}</div>
                 </span>
                 </span>
-        </div>)}
-    </div>
+            </div>)}
+        </div>
+    }
+
 }
 
 export default Users;
