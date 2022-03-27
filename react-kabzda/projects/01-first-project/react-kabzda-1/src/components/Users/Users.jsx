f45244b0e4b885416ab598049f314b36c63e58a6
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-03-27 17:53:10 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Users/Users.jsx
@@ -12,7 +12,17 @@ class Users extends  React.Component {
     }
 
     render() {
+
+            let pagesCount = this.props.totalUsersCount / this.props.pageSize;
+
        return <div>
+           <div>
+               <span>1</span>
+               <span className={styles.selectedPage}>2</span>
+               <span>3</span>
+               <span>4</span>
+               <span>5</span>
+           </div>
             <button onClick = {this.getUsers}>Get Users</button>
             {this.props.users.map(u => <div key={u.id}>
 <span>
