331390681e0e95b5c4254730e51225a369909fc8
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-04-09 01:40:57 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/api/api.js
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/api/api.js
@@ -1,15 +1,12 @@
 import * as axios from "axios";
 
-const instance = axios.create ({
-
+const instance = axios.create({
     withCredentials: true,
     baseURL: 'https://social-network.samuraijs.com/api/1.0/',
     headers: {
         "API-KEY": "0c3421b2-f0fc-40e8-b30f-6157d16d7c94"
     }
-
 });
-
 export const usersAPI = {
     getUsers(currentPage = 1, pageSize = 10) {
         return instance.get(`users?page=${currentPage}&count=${pageSize}`,)
@@ -18,8 +15,6 @@ export const usersAPI = {
             });
     }
 }
-
-
 export const getUsers2 = (currentPage = 1, pageSize = 10) => {
     return instance.get(`follow?page=${currentPage}&count=${pageSize}`,)
         .then(response => {
