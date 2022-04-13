349f743a50a65ed54e3bbb192121da0a29e13e9a
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-04-13 18:18:45 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Profile/ProfileContainer.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Profile/ProfileContainer.jsx
@@ -4,6 +4,7 @@ import {getUserProfile} from "../../redux/profile-reducer";
 import {Redirect, withRouter} from "react-router-dom";
 import {connect} from "react-redux";
 import {withAuthRedirect} from "../../hoc/withAuthRedirect";
+import {compose} from "redux";
 
 
 class ProfileContainer extends React.Component {
@@ -22,12 +23,14 @@ render () {
 }
 }
 
-let AuthRedirectComponent = withAuthRedirect(ProfileContainer);
-
 let mapStateToProps = (state) => ({
     profile: state.profilePage.profile,
 });
 
-let WithUrlDataContainerComponent = withRouter(AuthRedirectComponent)
+export default compose(
+    connect(mapStateToProps, {getUserProfile}),
+    withRouter,
+    withAuthRedirect,
+)(ProfileContainer);
 
-export default connect(mapStateToProps, {getUserProfile}) (WithUrlDataContainerComponent, ProfileContainer);
+
