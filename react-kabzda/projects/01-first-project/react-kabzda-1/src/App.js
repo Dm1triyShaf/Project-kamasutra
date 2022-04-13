9e6b3e9ad72e1b03ef26a506404a7510b446f7d8
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-04-13 15:24:52 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/App.js
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/App.js
@@ -9,7 +9,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
-
+import LoginPage from "./components/Login/Login";
 
 const App = () => {
       return (
@@ -29,6 +29,8 @@ const App = () => {
                                      render={ () =><Music />} />
                               <Route path='/settings/'
                                      render={ () => <Settings />} />
+                              <Route path='/login/'
+                                     render={ () => <LoginPage />} />
                   </div>
             </div>
       );
