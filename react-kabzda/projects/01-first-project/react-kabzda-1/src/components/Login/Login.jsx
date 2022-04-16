f1d7a756ee7dcfd1d33fd131f67a5c4175c1c046
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-04-16 21:43:13 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Login/Login.jsx
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/components/Login/Login.jsx
@@ -1,22 +1,39 @@
 import React from "react";
+import {Field, reduxForm} from "redux-form";
 
-const Login =(props) => {
-return <div>
-    <h1>Login</h1>
-    <form>
-        <div>
-            <input placeholder={"Login"}/>
-        </div>
-        <div>
-            <input placeholder={"Password"}/>
-        </div>
-        <div>
-            <input type={"checkbox"}/> remember me
-        </div>
-        <div>
-            <button>Login</button>
-        </div>
-    </form>
-</div>
+const LoginForm = (props) => {
+    return (
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"Login"} component={"input"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"Password"} component={"input"}/>
+            </div>
+            <div>
+                <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
+
+
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+
+    return (
+    <div>
+        <h1>Login</h1>
+        <LoginReduxForm onSubmit={onSubmit}/>
+    </div>
+    )
 }
 export default Login
