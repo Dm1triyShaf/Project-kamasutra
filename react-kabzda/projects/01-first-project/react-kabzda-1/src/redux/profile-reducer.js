ca75501b240d86b674fecde66f51b057d2f28469
Dmitriy Shafalyuk <Dima.shafalyuk@mail.ru>
2022-05-01 23:45:17 +0300

    Context API

--- react-kabzda/projects/01-first-project/react-kabzda-1/src/redux/profile-reducer.js
+++ react-kabzda/projects/01-first-project/react-kabzda-1/src/redux/profile-reducer.js
@@ -1,7 +1,6 @@
 import {profileAPI, usersAPI} from "../api/api";
 
 const ADD_POST = 'ADD-POST'
-const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
 const SET_STATUS_ = 'SET_STATUS'
 
@@ -10,7 +9,6 @@ let initialState = {
             { id: 1, message: 'Hi, how are you ?', likesCount: 15 },
             { id: 1, message: 'It\'s my first post', likesCount: 18 },
         ],
-        newPostText: 'gigi',
         profile: null,
         status:""
     };
@@ -20,7 +18,7 @@ const profileReducer = (state = initialState, action) => {
         case ADD_POST: {
             let newPost = {
                 id: 5,
-                message: state.newPostText,
+                message: action.newPostText,
                 likesCount: 0,
             };
             let stateCopy = {
@@ -29,12 +27,6 @@ const profileReducer = (state = initialState, action) => {
                 newPostText: ''
             };
             return stateCopy;
-        }
-        case UPDATE_NEW_POST_TEXT: {
-            return  {
-                ...state,
-                newPostText: action.newText
-            }
         }
         case SET_USER_PROFILE: {
             return {
@@ -52,7 +44,7 @@ const profileReducer = (state = initialState, action) => {
     }
 }
 
-export const addPostActionCreator = () => ({ type: ADD_POST })
+export const addPostActionCreator = (newPostText) => ({ type: ADD_POST,newPostText })
 export const setUserProfile = (profile) => ({ type: SET_USER_PROFILE, profile})
 export const setStatus = (status) => ({ type: SET_STATUS_, status})
 export const getUserProfile = (userId) => (dispatch) => {
@@ -74,7 +66,4 @@ export const updateStatus = (status) => (dispatch) => {
             }
     });
 }
-export const updateNewPostTextActionCreator = (text) =>
-    ({ type: UPDATE_NEW_POST_TEXT, newText: (text) })
-
 export default profileReducer;
